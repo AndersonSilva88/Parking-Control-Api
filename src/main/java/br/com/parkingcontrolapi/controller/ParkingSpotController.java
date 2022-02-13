f723dbfe41295446a6dd779aea8544832efc2e6c
Anderson <anderson.os88@hotmail.com>
2022-02-13 18:57:04 -0300

    projeto finalizado

--- src/main/java/br/com/parkingcontrolapi/controller/ParkingSpotController.java
+++ src/main/java/br/com/parkingcontrolapi/controller/ParkingSpotController.java
@@ -1,12 +1,90 @@
 package br.com.parkingcontrolapi.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.parkingcontrolapi.dto.ParkingSpoDTO;
+import br.com.parkingcontrolapi.model.ParkingSpotModel;
+import br.com.parkingcontrolapi.service.ParkingSpotService;
+import net.bytebuddy.TypeCache;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/parking-spot")
 public class ParkingSpotController {
+
+    final ParkingSpotService parkingSpotService;
+
+    public ParkingSpotController(ParkingSpotService parkingSpotService) {
+        this.parkingSpotService = parkingSpotService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpoDTO parkingSpoDTO) {
+        if (parkingSpotService.existsByLicensePlateCar(parkingSpoDTO.getLicensePlateCar())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: License Plate Car is already in use!");
+        }
+        if (parkingSpotService.existsByParkingSpotNumber(parkingSpoDTO.getParkingSpotNumber())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot is already in use!");
+        }
+        if (parkingSpotService.existsByApartmentAndBlock(parkingSpoDTO.getApartment(), parkingSpoDTO.getBlock())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot already registered for this apartament/block");
+        }
+
+        var parkingSpotModel = new ParkingSpotModel();
+        BeanUtils.copyProperties(parkingSpoDTO, parkingSpotModel);
+        parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ParkingSpotModel>> getAllParkingSpot(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneParkingSpot(@PathVariable(value = "id")UUID id){
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+        if (!parkingSpotModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found. ");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteParkingSpot(@PathVariable(value = "id") UUID id) {
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+        if (!parkingSpotModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking spot not found");
+        }
+        parkingSpotService.delete(parkingSpotModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Parking Spot deleted successfully.");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateParkingSpot(@PathVariable(value = "id") UUID id,
+                                                    @RequestBody @Valid ParkingSpoDTO parkingSpoDTO){
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+        if (!parkingSpotModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found. ");
+        }
+       var parkingSpotModel = new ParkingSpotModel();
+        BeanUtils.copyProperties(parkingSpoDTO, parkingSpotModel);
+        parkingSpotModel.setId(parkingSpotModelOptional.get().getId());
+        parkingSpotModel.setRegistrationDate(parkingSpotModelOptional.get().getRegistrationDate());
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModel));
+    }
 
 }
--- src/main/java/br/com/parkingcontrolapi/dto/ParkingSpoDTO.java
+++ src/main/java/br/com/parkingcontrolapi/dto/ParkingSpoDTO.java
@@ -1,49 +1,27 @@
-package br.com.parkingcontrolapi.model;
+package br.com.parkingcontrolapi.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
-import javax.persistence.*;
-import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.util.UUID;
+public class ParkingSpoDTO {
 
-@Entity
-@Table(name = "TB_PARKING_SPOT")
-public class ParkingSpotModel implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
-    @Column(nullable = false, unique = true, length = 10)
+    @NotBlank
     private String parkingSpotNumber;
-    @Column(nullable = false, unique = true, length = 7)
+    @NotBlank
+    @Size(max = 7)
     private String licensePlateCar;
-    @Column(nullable = false, length = 70)
+    @NotBlank
     private String brandCar;
-    @Column(nullable = false, length = 70)
+    @NotBlank
     private String modelCar;
-    @Column(nullable = false, length = 70)
+    @NotBlank
     private String colorCar;
-    @Column(nullable = false)
-    private LocalDateTime registrationDate;
-    @Column(nullable = false, length = 130)
+    @NotBlank
     private String responsibleName;
-    @Column(nullable = false, length = 30)
+    @NotBlank
     private String apartment;
-    @Column(nullable = false, length = 30)
+    @NotBlank
     private String block;
-
-    public ParkingSpotModel() {
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
 
     public String getParkingSpotNumber() {
         return parkingSpotNumber;
@@ -83,14 +61,6 @@ public class ParkingSpotModel implements Serializable {
 
     public void setColorCar(String colorCar) {
         this.colorCar = colorCar;
-    }
-
-    public LocalDateTime getRegistrationDate() {
-        return registrationDate;
-    }
-
-    public void setRegistrationDate(LocalDateTime registrationDate) {
-        this.registrationDate = registrationDate;
     }
 
     public String getResponsibleName() {
